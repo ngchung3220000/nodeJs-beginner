82f20da9185668a70f7677149bd8b97708a1e8af
ngchung <ngchung3220000@gmail.com>
2023-09-06 21:15:16 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+require("dotenv").config()
+
+const app = express()
+const port = process.env.PORT
+const hostname = process.env.HOST_NAME
+
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', "ejs");
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/ac', (req, res) => {
+    res.render("sample.ejs")
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`)
+})
